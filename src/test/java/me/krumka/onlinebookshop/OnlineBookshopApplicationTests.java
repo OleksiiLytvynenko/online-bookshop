dda05257595a2e1aa5c1fb73a63e321d2afa2c72
Oleksii Lytvynenko <oleksiila@gmail.com>
2025-05-05 23:52:19 +0100

    First commit

---
+++ src/test/java/me/krumka/onlinebookshop/OnlineBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.krumka.onlinebookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
