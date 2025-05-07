56833390e03267ad266b283c2462b18cb30fd5f8
Oleksii Lytvynenko <oleksiila@gmail.com>
2025-05-07 23:00:58 +0100

    Create repository, add Book entity, repository and service

---
+++ src/main/java/me/krumka/onlinebookshop/repository/BookRepositoryImpl.java
@@ -0,0 +1,52 @@
+package me.krumka.onlinebookshop.repository;
+
+import jakarta.persistence.criteria.CriteriaQuery;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import me.krumka.onlinebookshop.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+    @Autowired
+    private final SessionFactory sessionFactory;
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cannot insert book:" + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<Book> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Book.class);
+            criteriaQuery.from(Book.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot find all books", e);
+        }
+    }
+}
