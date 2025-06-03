4a1b556510b0a33d0b3b1b196a198be5aa3187e7
Oleksii Lytvynenko <oleksiila@gmail.com>
2025-06-03 20:21:47 +0100

    Add @ResponseStatus(HttpStatus.CREATED) to BookController POST endpoint

--- src/main/java/me/krumka/onlinebookshop/controller/BookController.java
+++ src/main/java/me/krumka/onlinebookshop/controller/BookController.java
@@ -5,16 +5,18 @@ import lombok.RequiredArgsConstructor;
 import me.krumka.onlinebookshop.dto.BookDto;
 import me.krumka.onlinebookshop.dto.CreateBookRequestDto;
 import me.krumka.onlinebookshop.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -29,6 +31,7 @@ public class BookController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
